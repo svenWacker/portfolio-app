900ed4fa2e89bc50f8bd32efe0463b54abb37f94
svenWacker <s.u.wacker@outlook.de>
2021-06-17 13:00:57 +0200

    folder-work and skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -20,8 +20,47 @@ const Skills = ({ data }) => {
   return (
     <React.Fragment>
       <h2>Skills</h2>
-      <div>
-        <ul className="skills">{items}</ul>
+      <div className="skills-wrapper">
+        <div className="list">
+          <ul className="skills">{items}</ul>
+        </div>
+        <div className="items">
+          <h3 className="technologyTitle">Technologies</h3>
+          <div class="technologies-container">
+            <img
+              src={`${process.env.PUBLIC_URL}/technologies/css.png`}
+              alt="css"
+            />
+            <img
+              src={`${process.env.PUBLIC_URL}/technologies/github.png`}
+              alt="github"
+            />
+            <img
+              src={`${process.env.PUBLIC_URL}/technologies/html-5.png`}
+              alt="html-5"
+            />
+            <img
+              src={`${process.env.PUBLIC_URL}/technologies/java-script.png`}
+              alt="java-script"
+            />
+            <img
+              src={`${process.env.PUBLIC_URL}/technologies/nodemon.svg`}
+              alt="nodemon"
+            />
+            <img
+              src={`${process.env.PUBLIC_URL}/technologies/slack.png`}
+              alt="slack"
+            />
+            <img
+              src={`${process.env.PUBLIC_URL}/technologies/vsc.png`}
+              alt="vsc"
+            />
+            <img
+              src={`${process.env.PUBLIC_URL}/technologies/zoom.png`}
+              alt="zoom"
+            />
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
